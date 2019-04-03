77659c9910f9b7cf249d1544e9638c552afef617
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-03 09:52:02 +0200

    exported Logo to Logo.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Logo from "./components/Logo";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Logo />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
---
+++ src/components/Logo.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import logo from "../logo.svg";
+
+export default function Logo() {
+  return <img src={logo} className="App-logo" alt="logo" />;
+}
