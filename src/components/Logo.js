906a40a035f5cb68a538ddf7c05c9849f84ee284
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-03 09:57:12 +0200

    changed to arrow function

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "../logo.svg";
 
-export default function Logo() {
+const Logo = () => {
   return <img src={logo} className="App-logo" alt="logo" />;
-}
+};
+export default Logo;
