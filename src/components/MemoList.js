05ec678ed61012cac072710090712854d616aa5d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-03 10:32:26 +0200

    styled MemoList in MemoList.css

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import "./MemoList.css";
 
-export class MemoList extends Component {
+class MemoList extends Component {
   render() {
     return (
       <div>
         <p>What do you want to add?</p>
+        <form>
+          <input type="text" name="item" id="add-item" />
+          <button>Add</button>
+        </form>
       </div>
     );
   }
