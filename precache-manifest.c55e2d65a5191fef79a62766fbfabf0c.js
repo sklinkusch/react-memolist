812fd9ae3d773fe89438e3c1d5420897d0774095
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-03 13:36:46 +0200

    Updates

---
+++ precache-manifest.c55e2d65a5191fef79a62766fbfabf0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-memolist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3738d567889df8ef8308",
+    "url": "/react-memolist/static/js/runtime~main.cb4315b7.js"
+  },
+  {
+    "revision": "1aa10d1aa1e48dd49665",
+    "url": "/react-memolist/static/js/main.337fd47a.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/react-memolist/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "1aa10d1aa1e48dd49665",
+    "url": "/react-memolist/static/css/main.a2886874.chunk.css"
+  },
+  {
+    "revision": "3c25db04667ae598aac2909ce7a80e23",
+    "url": "/react-memolist/index.html"
+  }
+];
