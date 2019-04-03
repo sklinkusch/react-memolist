af488e1dfaf0f94abe6d95d7260b6f7481529e81
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-03 11:48:14 +0200

    add functionality to the button

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -12,18 +12,31 @@ class MemoList extends Component {
   handleChange = event => {
     this.setState({ text: event.target.value });
   };
+  handleSubmit = event => {
+    console.log("Submit");
+    event.preventDefault();
+    const newItem = {
+      id: Date.now(),
+      text: this.state.text
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ""
+    }));
+  };
   render() {
     return (
       <div>
         <p>What do you want to add?</p>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             name="item"
             id="add-item"
             onChange={this.handleChange}
+            value={this.state.text}
           />
-          <button>Add</button>
+          <button>Add #{this.state.items.length + 1}</button>
         </form>
       </div>
     );
