130759f29ca80b848b0aff6b51323cbaa9bbdb4f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-04-03 10:19:30 +0200

    added label

---
+++ src/components/MemoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class MemoList extends Component {
+  render() {
+    return (
+      <div>
+        <p>What do you want to add?</p>
+      </div>
+    );
+  }
+}
+
+export default MemoList;
